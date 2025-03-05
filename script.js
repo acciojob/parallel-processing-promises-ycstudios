0ea7dfa8141e112e6eb27b0b85c9778d4ecebbf8
Yash Chandanshive <66194004+ycstudios@users.noreply.github.com>
2025-03-05 19:36:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,25 @@
-//your JS code here. If required.
-const output = document.getElementById("output");
-const btn = document.getElementById("download-images-button");
+const output = document.getElementById('output');
 
-const images = [
-  { url: "https://picsum.photos/id/237/200/300" },
-  { url: "https://picsum.photos/id/238/200/300" },
-  { url: "https://picsum.photos/id/239/200/300" },
-];
+let arr = [1, 2, 3, 4];
+
+let p1 = new Promise((resolve) => 
+    setTimeout(() => {
+        resolve(arr);
+    }, 3000)
+).then((arr) => {
+    return new Promise((resolve) => {
+        const filteredResult = arr.filter((value) => value % 2 === 0);
+        setTimeout(() => {
+            output.innerHTML + = `Filtered Result: ${filteredResult}`;
+            resolve(filteredResult);
+        }, 1000);
+    });
+}).then((filteredResult) => {
+    return new Promise((resolve) => {
+        const multipliedResult = filteredResult.map((value) => value * 2);
+        setTimeout(() => {
+            output.innerHTML+ = `Multiplied Result: ${multipliedResult}`;
+            resolve(multipliedResult);
+        }, 2000);
+    });
+})
